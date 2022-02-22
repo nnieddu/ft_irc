df6858f86bf7d0e9c55bef8720ff48edc90610e2
nnieddu <nicolas.nieddu@free.fr>
2022-02-22 22:36:45 +0100

    more oo code

--- Server.cpp
+++ Server.cpp
@@ -6,7 +6,7 @@
 /*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 14:42:26 by ninieddu          #+#    #+#             */
-/*   Updated: 2022/02/22 18:03:50 by ninieddu         ###   ########lyon.fr   */
+/*   Updated: 2022/02/22 22:29:37 by ninieddu         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,36 +23,35 @@ int server::getSock() const { return _socket; }
 int server::create_tcp_server_socket(int port) 
 {
 	struct sockaddr_in saddr;
-	int fd, ret_val;
+	int fd; 
+	int	ret_val;
 
 	fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (fd == -1) 
 	{
-		fprintf(stderr, "socket failed [%s]\n", strerror(errno));
+		std::cerr << "socket failed [" << strerror(errno) << "]" << std::endl;
 		close(fd);
 		return -1;
 	}
-	printf("Created a socket with fd: %d\n", fd);
-
 	/* Initialize the socket address structure */
 	saddr.sin_family = AF_INET;         
 	saddr.sin_port = htons(port); 
 	saddr.sin_addr.s_addr = INADDR_ANY;
-
-	/* Step2: bind the socket to port on the local host */
+	
+	/* bind the socket to port on the local host */
 	ret_val = bind(fd, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in));
 	if (ret_val != 0) 
 	{
-		fprintf(stderr, "bind failed [%s]\n", strerror(errno));
+		std::cerr << "bind failed [" << strerror(errno) << "]" << std::endl;
 		close(fd);
 		return -1;
 	}
-
-	/* Step3: listen for incoming connections */
+	
+	/* listen for incoming connections */
 	ret_val = listen(fd, 5);
 	if (ret_val != 0) 
 	{
-		fprintf(stderr, "listen failed [%s]\n", strerror(errno));
+		std::cerr << "listen failed [" << strerror(errno) << "]" << std::endl;
 		close(fd);
 		return -1;
 	}
--- main.cpp
+++ main.cpp
@@ -6,56 +6,43 @@
 /*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 14:42:03 by ninieddu          #+#    #+#             */
-/*   Updated: 2022/02/22 18:07:25 by ninieddu         ###   ########lyon.fr   */
+/*   Updated: 2022/02/22 22:35:51 by ninieddu         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-// TEST CODE FROM CODE BISON : http://www.codingbison.com/c/c-sockets-connection-oriented.html
-#include <stdio.h>
-#include <errno.h> 
-#include <netinet/in.h> 
-#include <sys/socket.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-#define DATA_BUFFER 5000
-
 int	start_server(int port, std::string password)
 {
 	server irc_server(port, password);
-	
+		
 	struct sockaddr_in new_client_addr;
 	int new_fd;
 	int ret_val = 1;
 	socklen_t addrlen = 0;
-	char buf[DATA_BUFFER];
+	char buf[5000];
 
-	/* Create the server socket */
-	// fd = create_tcp_server_socket(port); 
 	if (irc_server.getSock() == -1) 
 	{
-		fprintf(stderr, "Creating server failed [%s]\n", strerror(errno));
+		std::cerr << "Creating server failed  [" << strerror(errno) << "]" << std::endl;
 		return -1;
 	}
-
+	
 	/* Accept a new connection */
 	new_fd = accept(irc_server.getSock(), (struct sockaddr*)&new_client_addr, &addrlen);
 	if (new_fd == -1) 
 	{
-		fprintf(stderr, "accept failed [%s]\n", strerror(errno));
-		close(irc_server.getSock());
+		std::cerr << "accept failed [" << strerror(errno) << "]" << std::endl;
 		return -1;
 	}
-	printf("Accepted a new connection with fd: %d\n", new_fd);
+	
+	std::cout << "Accepted a new connection with fd : " << new_fd << std::endl;
 
 	/* Receive data */
 	std::cout << "Let us wait for the client to send some data" << std::endl;
 	while (ret_val > 0)
 	{
-		ret_val = recv(new_fd, buf, DATA_BUFFER, 0);
+		ret_val = recv(new_fd, buf, sizeof(buf), 0);
 		if (ret_val > 0) 
 			std::cout << "Received data: " << buf << std::endl;
 		if (ret_val == -1) 
@@ -68,7 +55,7 @@ int	start_server(int port, std::string password)
 	return(ret_val);
 }
 
-int	basic_check(int port, std::string pass)
+int	input_check(int port, std::string pass)
 {
 	if (port <= 0 || port > 65535)
 	{
@@ -94,7 +81,7 @@ int main(int ac, char **av)
 	int port = atoi(av[1]);
 	std::string password(av[2]);
 
-	if (basic_check(port, password))
+	if (input_check(port, password))
 		return(1);
 	
 	return(start_server(port, password));
