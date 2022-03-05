e6a89e5c7b29e0bcf3e2b2b3042e50c18a380fe4
lfourage <ludogriph@gmail.com>
2022-03-05 13:26:31 +0100

    RFC entry in comment, MODE +o part has me worried

--- srcs/Commands/Oper.cpp
+++ srcs/Commands/Oper.cpp
@@ -43,3 +43,31 @@ void Oper::execute()
 	_serv->getUser(*usr)->promoteServ();
 	_serv->send_replies(_expeditor, "You are now an operator", RPL_YOUREOPER);
 }
+
+/*
+4.1.5 Oper
+
+      Command: OPER
+   Parameters: <user> <password>
+
+   OPER message is used by a normal user to obtain operator privileges.
+   The combination of <user> and <password> are required to gain
+   Operator privileges.
+
+   If the client sending the OPER command supplies the correct password
+   for the given user, the server then informs the rest of the network
+   of the new operator by issuing a "MODE +o" for the clients nickname.
+
+   The OPER message is client-server only.
+
+   Numeric Replies:
+
+           ERR_NEEDMOREPARAMS              RPL_YOUREOPER
+           ERR_NOOPERHOST                  ERR_PASSWDMISMATCH
+
+   Example:
+
+   OPER foo bar                    ; Attempt to register as an operator
+                                   using a username of "foo" and "bar" as
+                                   the password.
+*/
