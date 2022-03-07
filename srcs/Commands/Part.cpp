3d6b4871b738351d2f2b1891b8fa77397568f9f8
nnieddu <nicolas.nieddu@free.fr>
2022-03-07 23:27:40 +0100

    PART clean

--- srcs/Commands/Part.cpp
+++ srcs/Commands/Part.cpp
@@ -21,7 +21,6 @@ Part::Part(server * serv):Command(serv)
 void Part::execute()
 {
 	std::string	*		channel = _args[CHANNEL].arg;
-	std::deque<std::string>	lst;
 
 	if (!channel)
 	{
@@ -53,26 +52,5 @@ void Part::execute()
 		_serv->remove_user_from(_expeditor, *it, "PART");
 		list.pop_front();
 	}
-	// std::string	*	arg = _args[CHANNEL].arg;
-	// std::cout << "PART ARG =" << *arg << std::endl;
-	// if (!arg)
-	// {
-	// 	_serv->send_replies(_expeditor, "PART :Not enough parameters", ERR_NEEDMOREPARAMS);
-	// 	return ;
-	// }
-	// *arg = nameCaseIns(*arg);
-	// if (_serv->channels.find(*arg) == _serv->channels.end())
-	// {
-	// 	_serv->send_replies(_expeditor, *arg + " :No such channel", ERR_NOSUCHCHANNEL);
-	// 	return ;
-	// }
-	// if (!_expeditor->isMember(*arg))
-	// {
-	// 	_serv->send_replies(_expeditor, *arg + " :You're not on that channel", ERR_NOTONCHANNEL);
-	// 	return ;
-	// }
-	// std::string quit = ":" + _expeditor->getNickname() + " PART " + *arg + "\r\n";
-	// send(_expeditor->getSock(), quit.c_str(), quit.length(), 0);
-	// _serv->remove_user_from(_expeditor, *arg, "PART");
 	return ;
 }
