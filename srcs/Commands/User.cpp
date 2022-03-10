7367d26c5cde7ca1107769b2c973ad416509d8a3
mrozniec <maximilien.rozniecki@gmail.com>
2022-03-10 11:50:53 +0100

    command USER functional with all param

--- srcs/Commands/User.cpp
+++ srcs/Commands/User.cpp
@@ -14,15 +14,20 @@ User & User::operator=(const User & x)
 User::User(server * serv):Command(serv)
 {
 	_args[NICK].isNeeded = true;
+	_args[CHANNEL].isNeeded = true;
+	_args[USER].isNeeded = true;
+	_args[MESSAGE].isNeeded = true;
 }
 
 /*----------------------------------------------------------------------------*/
 
 void User::execute()
 {
-	std::string	*	arg = _args[NICK].arg;
+	std::string	*	userName = _args[NICK].arg;
+	std::string	*	mode = _args[CHANNEL].arg;
+	std::string	*	rlName = _args[MESSAGE].arg;
 
-	if (!arg) 
+	if (!userName || !mode || !rlName)
 	{
 		_serv->send_replies(_expeditor, "USER :Not enough parameters", ERR_NEEDMOREPARAMS);
 		return ;
@@ -31,8 +36,17 @@ void User::execute()
 		_serv->send_replies(_expeditor, " :You may not reregister", ERR_ALREADYREGISTRED);
 	else if (_expeditor->getisLogged() != true && _expeditor->getLogLvl() == 2)
 	{
-		// _expeditor->setUsername(*arg);
-		_expeditor->setUsername(*arg + "@" + _expeditor->getHostname());
+		// _expeditor->setUsername(*userName);
+		_expeditor->setUsername(*userName + "@" + _expeditor->getHostname());
+		_expeditor->setRealname(*rlName);
+		if (*mode == "8")
+			_expeditor->setInvisible();
+		else if (*mode == "4")
+			_expeditor->setWallOp();
+		else if (*mode == "12") {
+			_expeditor->setWallOp();
+			_expeditor->setInvisible();
+		}
 		_expeditor->setLogged(true);
 		_serv->welcomeNewUser(_expeditor);
 	}
